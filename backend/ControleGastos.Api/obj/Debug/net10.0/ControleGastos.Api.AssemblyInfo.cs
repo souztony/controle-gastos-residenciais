170b20f9b9b1d64b94c0281df83c508a515a3c99
Tony Souza <tonysouz7@gmail.com>
2026-01-16 23:32:41 -0300

    feat: frontend

--- backend/ControleGastos.Api/obj/Debug/net10.0/ControleGastos.Api.AssemblyInfo.cs
+++ backend/ControleGastos.Api/obj/Debug/net10.0/ControleGastos.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ControleGastos.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+673ffc69165eb31a09374b9028215e94122197c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d94ee1a4a72e84e61f183e51b488de2b792a80ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("ControleGastos.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ControleGastos.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
